35b52564c0ee5fb91ee659ac84a85d03231e7501
angular <1921934563@qq.com>
2019-07-03 11:58:31 +0800

    添加项目文件。

---
+++ OW.Operation/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OW.Operation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult login()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult register()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult aaaa()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ OW.Operation/Filter/LoginAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OW.Operation.Filter
+{
+    public class LoginAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// 检测是否登录全局过滤器 原理：Action过滤器
+        /// </summary>
+        public bool IsCheck { get; set; }//IsCheck用于不需要检测的界面 的字段
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            if (IsCheck)
+            {
+                //检测用户是否登录
+                if (filterContext.HttpContext.Session["user"] == null)
+                {
+                    filterContext.HttpContext.Response.Redirect("/Home/Login");
+                }
+            }
+
+        }
+    }
+}
