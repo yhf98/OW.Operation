ea3ca30f233d3ad5527af406205d5eb99c4ef377
angular <1921934563@qq.com>
2019-07-06 02:00:30 +0800

    data

--- OW.Operation/OW.DAL/DBHelper.cs
+++ OW.Operation/OW.DAL/DBHelper.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Configuration;
 
 namespace OW.Operation.OW.DAL
 {
     public class DBHelper
     {
-        
+        private static string Sqlstring = ConfigurationManager.ConnectionStrings["SqlString"].ConnectionString.ToString();
     }
 }
