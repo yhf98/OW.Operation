5ec705d1e007d36d79b808518db3927b60f54650
吴定奇 <1347757229@qq.com>
2019-07-05 10:28:43 +0800

    页面

--- OW.Operation/Controllers/HomeController.cs
+++ OW.Operation/Controllers/HomeController.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace OW.Operation.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// 首页
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Svgdisplay()
+        {
+            return PartialView("Svgdisplay");
         }
         /// <summary>
         /// 登录
@@ -32,12 +41,45 @@ namespace OW.Operation.Controllers
 
             return View();
         }
-        public ActionResult aaaa()
+        /// <summary>
+        /// 会员车辆
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Member()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+        /// <summary>
+        /// 充值车辆
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Recharge()
+        {
+            ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        /// <summary>
+        /// 外来车辆
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Foreign()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        /// <summary>
+        /// 收入统计
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Statistics()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
